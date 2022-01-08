ee4a113434cc1f76836b186dde03ea76477e72eb
georgeiliadis91 <george.iliadis.91@gmail.com>
2022-01-08 23:01:13 +0200

    removed unused code

--- src/context/reducers/index.js
+++ src/context/reducers/index.js
@@ -1,34 +1,33 @@
 export const cartReducer = (state, action) => {
-	switch (action.type) {
-		case 'ADD_PRODUCT':
-			console.log('run');
-			return addItem(state, action.payload);
-		case 'REMOVE_PRODUCT':
-			return removeItem(state, action.payload);
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return addItem(state, action.payload);
+    case "REMOVE_PRODUCT":
+      return removeItem(state, action.payload);
+    default:
+      return state;
+  }
 };
 
 //FUNCTIONS
 const addItem = (state, id) => {
-	const exists = state.find((item) => item.id == id);
+  const exists = state.find((item) => item.id === id);
 
-	if (!exists) {
-		return (state = [...state, { id: id, amount: 1 }]);
-	} else {
-		const data = state.filter((prod) => {
-			return prod.id != id;
-		});
+  if (!exists) {
+    return (state = [...state, { id: id, amount: 1 }]);
+  } else {
+    const data = state.filter((prod) => {
+      return prod.id != id;
+    });
 
-		return (state = [...data, { id: exists.id, amount: exists.amount + 1 }]);
-	}
+    return (state = [...data, { id: exists.id, amount: exists.amount + 1 }]);
+  }
 };
 
 const removeItem = (state, id) => {
-	const data = state.filter((product) => {
-		return product.id !== id;
-	});
+  const data = state.filter((product) => {
+    return product.id !== id;
+  });
 
-	return data;
+  return data;
 };
